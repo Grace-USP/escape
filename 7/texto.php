bc5cbf10bd5936276e0c3c02992dcc0e859076c1
João Fukuda <joao.fukuda@protonmail.com>
2021-03-31 13:34:41 -0300

    Adicionar contatos na ultima página do escape

--- 7/texto.php
+++ 7/texto.php
@@ -1,1 +1,27 @@
-<p>É isso, parabéns!<br>Você conseguiu escapar!</p>
+<style>
+a.social {
+margin: 0px 30px 0px 30px;
+font-size: 0.7em;
+vertical-align: middle;
+}
+
+a.social img {
+width: 40px;
+height: auto;
+margin-right: 10px;
+}
+</style>
+
+<p>É isso, parabéns!</p>
+<p>Você conseguiu escapar!</p>
+<p>Agora você pode seguir o Grace nas redes sociais. :)</p>
+<br>
+<div style="text-align: left; width: 40%">
+	<a class="social" href="https://www.instagram.com/graceusp/" target="_blank"><img src="../assets/instagram.png">@graceusp</a>
+	<br>
+	<div style="height: 20px"></div>
+	<a class="social" href="http://each.usp.br/petsi/grace/" target="_blank"><img src="../assets/globe.png">each.usp.br/petsi/grace/</a>
+	<br>
+	<div style="height: 20px"></div>
+	<a class="social" href="mailto://grace-each@usp.br" target="_blank"><img src="../assets/email.png">grace-each@usp.br</a>
+</div>
